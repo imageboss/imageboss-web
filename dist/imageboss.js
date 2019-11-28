6dac4247d04dfd760900f15772a832dcb0e17bc7
Igor Escobar <igor@joindrover.com>
2019-11-28 18:40:46 +0000

    Releasing 2.0.6

--- dist/imageboss.js
+++ dist/imageboss.js
@@ -120,7 +120,9 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
   function resolveWidth(img) {
     var width = getAttribute(img, 'width') || yieldValidSize(img.getAttribute('width'));
 
-    if (!width && img.clientWidth > 1) {
+    if (width) {
+      return width;
+    } else if (img.clientWidth > 1) {
       return img.clientWidth;
     }
 
